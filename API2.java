b12f79af05c206b4968cbf9b7b450cd5ac81d67d
The-Cherry-Man <157427595+The-Cherry-Man@users.noreply.github.com>
2024-02-05 14:22:15 -0500

    Add files via upload (#7)
    * Add files via upload
    
    * Update and rename API_2.java to API2.java
    
    ---------
    
    Co-authored-by: jonathan7170 <157428885+jonathan7170@users.noreply.github.com>

---
+++ API2.java
@@ -0,0 +1,8 @@
+public interface API2 {
+
+    //Takes in user's input
+   List read(UserNumber n);
+
+   //Writes to user's destination
+   Response write(UserNumber n, DelimeterOutput d, ConfigKey c);
+}
