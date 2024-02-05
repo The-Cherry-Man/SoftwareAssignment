d186826dd8a1e0ce1a602049fd187d48141de466
The-Cherry-Man <157427595+The-Cherry-Man@users.noreply.github.com>
2024-02-05 11:55:35 -0500

    Add files via upload (#2)
    * Add files via upload
    
    * Update Test.java
    
    * Update Test.java

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String args[]) {
+        int number = 99;
+        System.out.println(number);
+    }
+    private Test(){
+        //checkstyle
+    }
+}
