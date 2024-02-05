975ada563aff3828638abc10bed46504e21146a3
jonathan7170 <157428885+jonathan7170@users.noreply.github.com>
2024-02-05 14:16:13 -0500

    Update and rename API_2.java to API2.java

--- API_2.java
+++
@@ -1,8 +0,0 @@
-public interface API_2 {
-
-    //Takes in user's input
-   List Read(UserNumber N);
-
-   //Writes to user's destination
-   Response Write(UserNumber N, DelimeterOutput D, ConfigKey C);
-}
