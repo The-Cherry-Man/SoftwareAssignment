f7270eeba8d875dbf8f6b31e35fef7172b85041c
The-Cherry-Man <157427595+The-Cherry-Man@users.noreply.github.com>
2024-02-05 14:20:24 -0500

    Update and rename Main.java to API1.java

---
+++ API1.java
@@ -0,0 +1,21 @@
+public interface API1 {
+
+    //Takes in user number Data
+    ConfigKey userNumberInput(UserNumber n);
+
+
+    //Chooses the users Delimeter.
+    ConfigKey delimeterOuput(Delimeter d);
+
+
+    //Sets the destination of the data results
+    ConfigKey destinationOutput(Destination d);
+
+   Result compute(ConfigKeyCollection c);
+
+   //Delimeter Choices
+    char D1 = ';';
+    char D2 = '|';
+    char D3 = '/';
+
+}
