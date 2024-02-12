78f8f573d9ef0c08ab9d42fd74c6986eaaf95586
jonathan7170 <157428885+jonathan7170@users.noreply.github.com>
2024-02-12 16:23:04 -0500

    Create API1_Smoke_Test.java
    Smole test 1

---
+++ .test/API1_Smoke_Test.java
@@ -0,0 +1,55 @@
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class API1_Smoke_Test {
+
+    @Test
+    public void UserinputTest(){
+
+        UserNumber k = 13;
+
+        UserNetworkBoundaryAPI UNBA = new  UserNetworkBoundaryAPI();
+
+        ConfigKey useredNumberInput = UNBA.UserNumberInput(k);
+
+        Assert.assertNotNull(useredNumberInput);
+    }
+
+    @Test
+    public void DelimterTest(){
+
+        Delimeter d = '/';
+
+        UserNetworkBoundaryAPI UNBA = new  UserNetworkBoundaryAPI();
+
+        ConfigKey configKey = UNBA.DelimeterOuput(d);
+
+        Assert.assertNotNull(configKey);
+    }
+
+    @Test
+    public void DestinationTest(){
+
+        Destination s = "destination";
+
+        UserNetworkBoundaryAPI UNBA = new  UserNetworkBoundaryAPI();
+
+        ConfigKey destinationOutput = UNBA.DestinationOutput(s);
+
+        Assert.assertNotNull(destinationOutput);
+    }
+
+    @Test
+    public void Computetest(){
+
+        List<Integer> list = new Arraylist<>();
+
+        ConfigKeyCollection C = list;
+
+        UserNetworkBoundaryAPI UNBA = new  UserNetworkBoundaryAPI();
+
+        Result compute1 = UNBA.Compute(C);
+
+        Assert.assertNotNull(compute1);
+    }
+}
