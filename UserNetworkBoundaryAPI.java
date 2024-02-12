dbd74103281e570e2d4598391f406b80f6f7687f
jonathan7170 <157428885+jonathan7170@users.noreply.github.com>
2024-02-12 16:00:11 -0500

    Add files via upload

---
+++ UserNetworkBoundaryAPI.java
@@ -0,0 +1,30 @@
+public class UserNetworkBoundaryAPI implements API1{
+
+        private API2 api2;
+
+        private API3 api3;
+
+        public UserNetworkBoundaryAPI() {
+
+         }
+
+    @Override
+    public ConfigKey UserNumberInput(UserNumber N) {
+        return null;
+    }
+
+    @Override
+    public ConfigKey DelimeterOuput(Delimeter D) {
+        return null;
+    }
+
+    @Override
+    public ConfigKey DestinationOutput(Destination D) {
+        return null;
+    }
+
+    @Override
+    public Result Compute(ConfigKeyCollection C) {
+        return null;
+    }
+}
