af393cda36608e0406498431d394695897378dfc
The-Cherry-Man <157427595+The-Cherry-Man@users.noreply.github.com>
2024-02-12 16:42:01 -0500

    Add files via upload

---
+++ .test/ComputeEngineIntegrationTest.java
@@ -0,0 +1,38 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.print.attribute.standard.Destination;
+
+public class ComputeEngineIntegrationTest {
+    @Test
+    public void IntegrationTest() {
+
+        InMemoryDataStore imds = new InMemoryDataStore();
+        UserNumber n = imds.getInput();
+        DelimeterOutput d = '/';
+        Destination x = imds.getOutput();  //Should it be List<String>?
+        ConfigKey c = 1000;
+
+        UserNetworkBoundaryAPI unba = new UserNetworkBoundaryAPI;
+        unba.UserNumberInput(n);
+        unba.DelimeterOutput(d);
+        unba.DestinationOutput(x);
+        unba.ConfigKeyCollection(c);
+
+        DataProcessBoundaryAPI dpba = new DataProcessBoundaryAPI();
+        dpba.read(n);
+        dpba.write(n, d, c);
+
+        ComputeConceptualBoundaryAPI ccba = new ComputeConceptualBoundaryAPI();
+        int i = 5;
+        ccba.input(i);
+
+        Assert.assertNotNull(x);
+
+
+
+
+    }
+
+
+}
