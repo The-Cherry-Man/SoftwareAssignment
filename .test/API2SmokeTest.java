b14ab06c1428311fac943e9d2c855e9d4b786ab7
The-Cherry-Man <157427595+The-Cherry-Man@users.noreply.github.com>
2024-02-12 16:36:13 -0500

    Adding API2 smoke test file (#26)
    * Adding API2 smoke test file
    
    * Update and rename API2_Smoke_Test.java to API2SmokeTest.java
    
    ---------
    
    Co-authored-by: jonathan7170 <157428885+jonathan7170@users.noreply.github.com>

---
+++ .test/API2SmokeTest.java
@@ -0,0 +1,30 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.List;
+
+public class API2SmokeTest {
+
+    public API2SmokeTest(){
+        
+    }
+    
+    @Test
+    public void readTest(){
+        UserNumber k = 5;
+        DataProcessBoundaryAPI dpba = new DataProcessBoundaryAPI();
+        List read = dpba.read(k);
+        Assert.assertNotNull(read);
+    }
+    @Test
+    public void writeTest() {
+        UserNumber k = 5;
+        DelimeterOutput d = '/';
+        ConfigKey c = 1000;
+        DataProcessBoundaryAPI dpba = new DataProcessBoundaryAPI();
+        List read = dpba.read(k, d, c);
+        Assert.assertNotNull(read);
+    }
+
+}
