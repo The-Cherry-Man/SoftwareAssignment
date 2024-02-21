bd1eb5b9d2f0358b5d7a628a92b2e4fada044d86
The-Cherry-Man <157427595+The-Cherry-Man@users.noreply.github.com>
2024-02-21 11:41:04 -0500

    Update ComputeEngineAPI3.java

--- ComputeEngineAPI3.java
+++ ComputeEngineAPI3.java
@@ -24,7 +24,7 @@ public interface ComputeEngineAPI3 {
           int digit = factorial.toString().charAt(i) - '0';
           if(digit == 0){
             continue;
-          
+          }
           else{
               product = product.multiply(BigInteger.valueOf(digit));
           }
