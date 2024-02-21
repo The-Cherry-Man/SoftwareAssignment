4f4093564be1db59cb4fdde1f245a6c9909f1373
The-Cherry-Man <157427595+The-Cherry-Man@users.noreply.github.com>
2024-02-21 11:49:52 -0500

    Update ComputeEngineAPI3.java (#31)
    * Update ComputeEngineAPI3.java
    
    adds computation
    
    * Update ComputeEngineAPI3.java
    
    * Update ComputeEngineAPI3.java
    
    * Update ComputeEngineAPI3.java
    
    * Update ComputeEngineAPI3.java
    
    * Update ComputeEngineAPI3.java
    
    * Update ComputeConceptualBoundaryAPI.java
    
    updates implemented interface

--- .src/ComputeConceptualBoundaryAPI.java
+++ .src/ComputeConceptualBoundaryAPI.java
@@ -1,10 +1,41 @@
+import java.math.BigInteger;
 public class ComputeConceptualBoundaryAPI implements ComputeEngineAPI3 {
 
     public ComputeConceptualBoundaryAPI() {
 
     }
     @Override
-    public Integer input(Integer i){
-        return null;
-    }
+public interface ComputeEngineAPI3 {
+
+
+  public BigInteger computation(Integer i){
+    BigInteger firstResult = factorialResult(i);
+
+    BigInteger secondResult = factorialProduct(firstResult);
+
+    return secondResult;
+  }
+  static BigInteger factorialResult(int input) {
+      //Calculates factorial
+      BigInteger number = new BigInteger("1");
+      for(int i = 1; i <= input; i++) {
+          number = number.multiply(BigInteger.valueOf(i));
+      }
+      return number;
+  }
+  static BigInteger factorialProduct(BigInteger factorial) {
+      //Finds product of digits of factorial
+      BigInteger product = BigInteger.ONE;
+      for (int i = 0; i < factorial.toString().length(); i++) {
+          int digit = factorial.toString().charAt(i) - '0';
+          if(digit == 0){
+            continue;
+          }else{
+            product = product.multiply(BigInteger.valueOf(digit));
+          }
+      }
+      return product;
+  }
+
+}
 }
