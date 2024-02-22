4413b5a9f7fc010b8946c57f04331f0cfaa3ed2c
jonathan7170 <157428885+jonathan7170@users.noreply.github.com>
2024-02-22 17:49:25 -0500

    Add files via upload

---
+++ .src/Response.java
@@ -0,0 +1,17 @@
+public class Response {
+
+    boolean result = true;
+
+    public Response(){
+
+    }
+    public Response (boolean b){
+
+        this.result =b;
+    }
+    public boolean getResponse(){
+
+        return result;
+
+    }
+}
