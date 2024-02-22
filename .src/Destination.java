2ca1ebb82e29b8c4d68a8fb0dd075df23610ef3c
jonathan7170 <157428885+jonathan7170@users.noreply.github.com>
2024-02-22 18:01:26 -0500

    Add files via upload (#33)
    * Add files via upload
    
    * Update Destination.java
    
    * Update Destination.java
    
    * Update Destination.java
    
    fixed caps

---
+++ .src/Destination.java
@@ -0,0 +1,26 @@
+public class Destination {
+
+
+    String userDestination;
+
+    public Destination(){
+        
+    }
+
+    public Destination(String s){
+
+        this.userDestination = s;
+    }
+    
+    public String getDestination(){
+
+        return this.userDestination;
+
+    }
+    public void setDestination(String s){
+
+
+        this.userDestination = s;
+    }
+
+}
