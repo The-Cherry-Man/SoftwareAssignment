9e2a7873b9cb94f30f921e89cfc3fdc6b4ad5cca
jonathan7170 <157428885+jonathan7170@users.noreply.github.com>
2024-02-22 16:12:50 -0500

    Add files via upload

---
+++ .src/Destination.java
@@ -0,0 +1,25 @@
+public class Destination {
+
+
+    String UserDestination;
+
+    public Destination(){
+        
+    }
+
+    public Destination(String s){
+
+        this.UserDestination = s;
+    }
+    public String getDestination(){
+
+        return this.UserDestination;
+
+    }
+    public void setDestination(String s){
+
+
+        this.UserDestination = s;
+    }
+
+}
