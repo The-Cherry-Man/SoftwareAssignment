e5c760611ad7faac2e2633312dad3bc64e7dee01
The-Cherry-Man <157427595+The-Cherry-Man@users.noreply.github.com>
2024-02-22 22:36:24 -0500

    Add files via upload (#51)

---
+++ Response.java
@@ -0,0 +1,12 @@
+public class Response {
+    boolean result = true;
+    public Response(){
+
+    }
+    public Response(boolean b){
+        this.result = b;
+    }
+    public boolean getResponse(){
+        return result;
+    }
+}
