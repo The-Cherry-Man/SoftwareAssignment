ecddb2d9a70077c272ded8c977c3e97f562af67a
The-Cherry-Man <htlongx@gmail.com>
2024-02-29 12:42:07 -0500

    Delete CheckstyleTest.java
    not necessary

--- CheckstyleTest.java
+++
@@ -1,9 +0,0 @@
-public class CheckstyleTest {
-    public static void main(String args[]) {
-        int number = 99;
-        System.out.println(number);
-    }
-    private void Test(){
-        //checkstyle
-    }
-}
