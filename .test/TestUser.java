096938647e0b0f41cf7b20a32e25152c20df5806
The-Cherry-Man <htlongx@gmail.com>
2024-03-01 14:31:47 -0500

    Updated file searching
    txt file for the test was wrongly named so updated, also java file was looking for a "test" file so i changed it to look for a ".test" file. IntelliJ added more files again as well

--- .test/TestUser.java
+++ .test/TestUser.java
@@ -16,7 +16,7 @@ public class TestUser {
 
     public void run(String outputPath) throws IOException {
         char delimiter = ';';
-        String inputPath = "test" + File.separatorChar + "testInputFile.test";
+        String inputPath = ".test" + File.separatorChar + "testInputFile.txt";
 
         // TODO 4: Call the appropriate method(s) on the coordinator to get it to
         // run the compute job specified by inputPath, outputPath, and delimiter
