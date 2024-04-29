7e4e0a7bd0ed7a28df49d19bb717b59c16752da1
The-Cherry-Man <157427595+The-Cherry-Man@users.noreply.github.com>
2024-04-29 15:10:09 -0400

    Update TimingLoggingProxy.java
    added constructor

--- test/TimingLoggingProxy.java
+++ test/TimingLoggingProxy.java
@@ -4,6 +4,9 @@ import java.lang.reflect.Proxy;
 
 /** A wrapper around an interface that tracks the time each of its methods takes */
 public class TimingLoggingProxy {
+
+	public TimingLoggingProxy(){
+	}
 	
 	public static <T> T createProxy(T wrappedObject, Class<T> proxyClass, LoggingStatistics stats) {
 		return (T) Proxy.newProxyInstance(TimingLoggingProxy.class.getClassLoader(), new Class[] {proxyClass}, new InvocationHandler() {
