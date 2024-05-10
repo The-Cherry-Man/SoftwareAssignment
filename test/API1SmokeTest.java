26b88d3f16ea78368c79a81b6dbbb378350e5aa3
jonathan7170 <157428885+jonathan7170@users.noreply.github.com>
2024-05-10 11:29:41 -0400

    Update API1SmokeTest.java (#108)
    * Update API1SmokeTest.java
    
    Added parameters for UNBA instance variables
    
    * Update API1SmokeTest.java
    
    added import

--- test/API1SmokeTest.java
+++ test/API1SmokeTest.java
@@ -1,6 +1,6 @@
 import junit.framework.Assert;
-import org.junit.Test;
-
+//import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.math.BigInteger;
@@ -19,7 +19,7 @@ public class API1SmokeTest {
 
         FileUserNumber n = new FileUserNumber();
 
-        UserNetworkBoundaryAPI unba = new  UserNetworkBoundaryAPI();
+        UserNetworkBoundaryAPI unba = new  UserNetworkBoundaryAPI(new DataProcessBoundaryAPI(),new ComputeConceptualBoundaryAPI());
 
         ConfigKey userdNumberInput = unba.userNumberInput(n);
 
@@ -31,7 +31,7 @@ public class API1SmokeTest {
 
         Delimeter d = new Delimeter('/');
 
-        UserNetworkBoundaryAPI unba = new  UserNetworkBoundaryAPI();
+        UserNetworkBoundaryAPI unba = new  UserNetworkBoundaryAPI(new DataProcessBoundaryAPI(),new ComputeConceptualBoundaryAPI());
 
         ConfigKey configKey = unba.delimeterOuput(d);
 
@@ -45,7 +45,7 @@ public class API1SmokeTest {
 
         s.setDestination("destination");
 
-        UserNetworkBoundaryAPI unba = new  UserNetworkBoundaryAPI();
+        UserNetworkBoundaryAPI unba = new  UserNetworkBoundaryAPI(new DataProcessBoundaryAPI(),new ComputeConceptualBoundaryAPI());
 
         ConfigKey destinationOutput = unba.destinationOutput(s);
 
