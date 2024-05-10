b3fc81953b30ec3732182b42f192e825548e27c5
The-Cherry-Man <157427595+The-Cherry-Man@users.noreply.github.com>
2024-05-10 11:55:49 -0400

    Reuploading previous pull request (#115)
    Took changes from Jonathan's previous pull request to now work with new changes

--- src/ConfigKey.java
+++ src/ConfigKey.java
@@ -1,9 +1,14 @@
+import java.util.Objects;
+
 public class ConfigKey {
 
     private static int ConfigKey = 1;
 
     private int key;
+
     public ConfigKey(int i){
+
+        key = i;
 
     }
 
@@ -19,4 +24,23 @@ public class ConfigKey {
         return key;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o){
+
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()){
+
+            return false;
+        }
+        ConfigKey configKey = (ConfigKey) o;
+        return key == configKey.key;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key);
+    }
 }
